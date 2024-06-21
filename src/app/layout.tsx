8fafd7f8860cd6d83e7270bb90f88338bf20c6cc
teyim <teyimasobo@gmail.com>
2024-06-21 10:16:36 +0100

    style : remove grotesk font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <QueryClientContextProvider>
-        <body className={`${space_Grotesk.variable} font-grotesk`}>
+        <body className={`${space_Grotesk.variable}`}>
           <NextAuthProvider>{children}</NextAuthProvider>
         </body>
       </QueryClientContextProvider>

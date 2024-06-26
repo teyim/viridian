27e9e6974e4e7d7b69ca3c2d46c00b5b92bd2044
teyim <teyimasobo@gmail.com>
2024-06-26 22:48:01 +0100

    refactor: modify trees data

--- src/constants/trees.ts
+++ src/constants/trees.ts
@@ -1,8 +1,8 @@
 export const trees = [
   {
-    name: "Cheesy Oak",
+    name: "CodeGrove",
     description:
-      "A tiny sapling with vibrant yellow leaves, resembling a young cheese wedge.",
+      "A flourishing tree symbolizing the interconnectedness and complexity of code, with branches that reach out like pathways in software development..",
     rarity: "Common",
     xpThreshold: 0,
     imageUrl:
@@ -11,9 +11,9 @@ export const trees = [
     secondaryColor: "#FEF08A",
   },
   {
-    name: "Echowood",
+    name: "ByteBirch",
     description:
-      " A tree with green leaves that remembers every sound spoken near it, whispering echoes of the past to those who listen closely.",
+      "Known for its sturdy trunk and the intricate patterns on its bark, reflecting the foundational elements and logical structure essential in software architecture.",
     rarity: "Common",
     xpThreshold: 250,
     imageUrl:
@@ -22,9 +22,9 @@ export const trees = [
     secondaryColor: "#BBF7D0",
   },
   {
-    name: "Blushwood",
+    name: "SyntaxSpruce",
     description:
-      "A young tree with vibrant pink leaves, symbolizing rapid growth and new life.",
+      "Adorned with leaves that shimmer in various colors, embodying the structured elegance and clarity found in well-crafted code.",
     rarity: "Common",
     xpThreshold: 425,
     imageUrl:
@@ -33,58 +33,179 @@ export const trees = [
     secondaryColor: "#BBF7D0",
   },
   {
-    name: "Luminara Arborea",
+    name: "BugBamboo",
     description:
-      "A radiant tree with bioluminescent leaves that glow and shift colors, healing nearby creatures and creating luminous constructs.",
-    rarity: "Common",
-    xpThreshold: 800,
+      "A resilient species thriving amidst challenges, symbolizing adaptability and the continuous effort needed to debug and improve software.",
+    rarity: "Uncommon",
+    xpThreshold: 600,
     imageUrl:
       "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063657/_Group_-17_xnqzs7.svg",
     primaryColor: "#0891B2",
     secondaryColor: "#A5F3FC",
   },
   {
-    name: "Luminara Arborea",
+    name: "PixelPalm",
     description:
-      "A radiant tree with bioluminescent leaves that glow and shift colors, healing nearby creatures and creating luminous constructs.",
-    rarity: "Common",
-    xpThreshold: 1200,
+      "Resembling a digital oasis, its leaves flicker with pixelated brilliance, representing the meticulous detail and graphical precision in software interfaces.",
+    rarity: "Uncommon",
+    xpThreshold: 775,
     imageUrl:
       "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063667/_Group_-16_t1ocxk.svg",
     primaryColor: "#7C3AED",
     secondaryColor: "#DDD6FE",
   },
   {
-    name: "Luminara Arborea",
+    name: "MergeMaple",
     description:
-      "A radiant tree with bioluminescent leaves that glow and shift colors, healing nearby creatures and creating luminous constructs.",
-    rarity: "Common",
-    xpThreshold: 1200,
+      "With branches that merge gracefully, it embodies the collaborative nature of code development, where different streams of work converge.",
+    rarity: "Uncommon",
+    xpThreshold: 950,
     imageUrl:
       "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063668/_Group_-15_sditkp.svg",
     primaryColor: "#0284C7",
     secondaryColor: "#BAE6FD",
   },
   {
-    name: "Luminara Arborea",
+    name: "CommitCypress",
     description:
-      "A radiant tree with bioluminescent leaves that glow and shift colors, healing nearby creatures and creating luminous constructs.",
-    rarity: "Common",
-    xpThreshold: 1200,
+      "Rooted deeply in the codebase, it symbolizes stability and growth, nurtured by consistent commits that mark progress in software projects.",
+    rarity: "Rare",
+    xpThreshold: 1125,
     imageUrl:
       "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063674/_Group_-14_dpff7f.svg",
     primaryColor: "#DB2777",
     secondaryColor: "#FBCFE8",
   },
   {
-    name: "Luminara Arborea",
+    name: "VersionVine",
     description:
-      "A radiant tree with bioluminescent leaves that glow and shift colors, healing nearby creatures and creating luminous constructs.",
-    rarity: "Common",
-    xpThreshold: 1200,
+      "A climbing vine intertwining through growth stages, each node representing a version in the lifecycle of software, linking innovation with continuity.",
+    rarity: "Rare",
+    xpThreshold: 1300,
     imageUrl:
       "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063671/_Group_-13_ky8chu.svg",
     primaryColor: "#EA580C",
     secondaryColor: "#FED7AA",
+  },
+  {
+    name: "BranchBonsai",
+    description:
+      "Crafted meticulously, its miniature form reflects the artistry and strategic planning in branching strategies essential for software scalability.",
+    rarity: "Rare",
+    xpThreshold: 1475,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063677/_Group_-12_lbjhqj.svg",
+    primaryColor: "#CA8A04",
+    secondaryColor: "#FEF08A",
+  },
+  {
+    name: "ForkFir",
+    description:
+      "Known for its ability to branch into new paths, it embodies the spirit of exploration and innovation, fundamental in software development.",
+    rarity: "Rare",
+    xpThreshold: 1650,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063683/_Group_-11_cmx734.svg",
+    primaryColor: "#16A34A",
+    secondaryColor: "#BBF7D0",
+  },
+  {
+    name: "PullPine",
+    description:
+      "A majestic pine fostering collaboration, with branches inviting contributions and symbolizing the strength of community in open-source projects.",
+    rarity: "Epic",
+    xpThreshold: 1825,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063679/_Group_-10_sujlqy.svg",
+    primaryColor: "#16A34A",
+    secondaryColor: "#BBF7D0",
+  },
+  {
+    name: "RepoRedwood",
+    description:
+      "Towering and enduring, it provides a solid foundation for vast repositories of code, symbolizing resilience and support in software ecosystems.",
+    rarity: "Epic",
+    xpThreshold: 2000,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063682/_Group_-9_vsj3rt.svg",
+    primaryColor: "#0891B2",
+    secondaryColor: "#A5F3FC",
+  },
+  {
+    name: "CodeCactus",
+    description:
+      "Resilient and adaptive, thriving in diverse environments, symbolizing developers' ability to navigate challenges and grow in their coding journey.",
+    rarity: "Epic",
+    xpThreshold: 2175,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063703/_Group_-8_gnr7q7.svg",
+    primaryColor: "#7C3AED",
+    secondaryColor: "#DDD6FE",
+  },
+  {
+    name: "PushPalm",
+    description:
+      "With leaves swaying in the wind of progress, it encourages developers to push forward, embracing iterative improvements and advancements in software.",
+    rarity: "Epic",
+    xpThreshold: 2350,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063722/_Group_-7_apnbim.svg",
+    primaryColor: "#0284C7",
+    secondaryColor: "#BAE6FD",
+  },
+  {
+    name: "SyntaxWillow",
+    description:
+      "Graceful and flexible, its branches flow with creativity, embodying the fluidity and expressiveness of code syntax in software design.",
+    rarity: "Legendary",
+    xpThreshold: 2525,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063713/_Group_-6_jku1ds.svg",
+    primaryColor: "#DB2777",
+    secondaryColor: "#FBCFE8",
+  },
+  {
+    name: "BitBlossom",
+    description:
+      "Bursting with digital blooms, each representing a milestone or improvement in software development, celebrating achievements and progress.",
+    rarity: "Legendary",
+    xpThreshold: 2875,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063657/_Group_-5_yirpz7.svg",
+    primaryColor: "#EA580C",
+    secondaryColor: "#FED7AA",
+  },
+  {
+    name: "JavaJuniper",
+    description:
+      "Robust and versatile, supporting complex ecosystems, symbolizing the strength and scalability of Java in software applications.",
+    rarity: "Mythical",
+    xpThreshold: 3050,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063662/_Group_-4_f9w8sa.svg",
+    primaryColor: "#DB2777",
+    secondaryColor: "#FBCFE8",
+  },
+  {
+    name: "RubyRain",
+    description:
+      "Vibrant and dynamic, with leaves that sparkle like gems, symbolizing the elegance and power of Ruby on Rails frameworks in web development.",
+    rarity: "Mythical",
+    xpThreshold: 3225,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063726/_Group_-3_keumtz.svg",
+    primaryColor: "#EA580C",
+    secondaryColor: "#FED7AA",
+  },
+  {
+    name: "NodeNettle",
+    description:
+      "Adaptable and interconnected, thriving in networks, much like Node.js in scalable server-side applications and real-time environments.",
+    rarity: "Mythical",
+    xpThreshold: 3400,
+    imageUrl:
+      "https://res.cloudinary.com/dikehxjsi/image/upload/v1711063676/_Group_-2_bbpzfl.svg",
+    primaryColor: "#CA8A04",
+    secondaryColor: "#FEF08A",
   },
 ];
